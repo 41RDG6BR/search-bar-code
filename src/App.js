4006912faadfa4c4a205d094e95e71aa790d910d
41RDG6BR <rdg6design@gmail.com>
2022-03-15 15:28:37 -0300

    React autocomplete search from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './custom.css'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [text, setText] = useState('')
+  const [suggestions, setSuggestions] = useState([])
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+      console.log(response.data)
+      setUsers(response.data)
+    }
+    loadUsers()
+  }, [])
+
+  const onSuggestHandler = text => {
+    setText(text)
+  }
+
+  const onChangeHandler= text => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi")
+        return user.email.match(regex)
+      })
+    }
+    console.log(matches, 'matche')
+    setSuggestions(matches)
+    setText(text)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <input type="text"
+        onChange={(e => onChangeHandler(e.target.value))}
+        value={text}
+        onBlur={() => {
+          setSuggestions([])
+        }}
+      />
+      {suggestions && suggestions.map((suggestion, i) =>
+        <div
+          key={i}
+          className="suggestion"
+          onClick={() => onSuggestHandler(suggestion.email)}
         >
-          Learn React
-        </a>
-      </header>
+          {suggestion.email}
+        </div>
+      )}
     </div>
   );
 }
